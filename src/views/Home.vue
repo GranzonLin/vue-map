cbf50a4809a3bcd2a4af9b46dc3c5ade792d0f2d
granzon <1978882990@qq.com>
2020-05-11 22:10:15 +0800

    feat init project

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,53 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <div id="mapContainer" class="map-container" ref="mapContainer"></div>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'Home',
+        data(){
+            return{
+                zoom:'15',
+                T:{},
+                map:''
+            }
+        },
+        mounted(){
+            this.drawMap()
+        },
+        methods:{
+            // 确保地图加载完毕
+            init(){
+                return new Promise((resolve,reject) => {
+                    if(typeof window.T !== 'undefined'){
+                        resolve(window.T);
+                        return true
+                    }
+                    window.onload = () => {
+                        resolve(window.T)
+                    }
+                })
+            },
+            drawMap(){
+                this.init().then(T => {
+                    this.T = T;
+                    this.map = new T.Map(this.$refs.mapContainer);
+                    this.map.centerAndZoom(new T.LngLat(116.112230,24.304730), this.zoom);
+                })
+            },
+            getLngLat(lnglat) {
+                console.log(lnglat.lng.toFixed(6) + "," + lnglat.lat.toFixed(6));
+            }
+        }
+    }
 </script>
+
+<style lang="less" scoped>
+    .map-container{
+        width: 900px;
+        height: 700px;
+        margin: 0 auto;
+    }
+</style>
